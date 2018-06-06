304b645c3a16b0c746f87b7244f0cec27dea54c4
jrmelton50 <jrmelton@crimson.ua.edu>
2018-06-06 13:06:17 -0500

    removed mailgun

--- server/src/config/env/production.js
+++ server/src/config/env/production.js
@@ -6,7 +6,7 @@ let cfg = {
     DB_NAME: process.env.DB_NAME,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
-    MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
+    // MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
     // STRIPE_SK: process.env.STRIPE_SK
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
