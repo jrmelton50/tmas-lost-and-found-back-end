54aa691f0bacefb9ad49faf2a0ead6079a904508
jrmelton50 <jrmelton@crimson.ua.edu>
2018-06-06 15:22:06 -0500

    added mailgun

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -1,17 +1,17 @@
-// import mailgunLoader from 'mailgun-js';
-// import { config } from "../config"
+import mailgunLoader from 'mailgun-js';
+import { config } from "../config"
 
-// let mailgun = mailgunLoader({ apiKey: config.MAILGUN_API_KEY, domain: 'sandbox044092ed20d046f3b8b985d9c2fbe2d7.mailgun.org' });
+let mailgun = mailgunLoader({ apiKey: config.MAILGUN_API_KEY, domain: 'sandbox044092ed20d046f3b8b985d9c2fbe2d7.mailgun.org' });
 
-// function sendEmail(to, from, subject, content) {
-//     let data = {
-//         from,
-//         to,
-//         subject,
-//         html: content
-//     };
+function sendEmail(to, from, subject, content) {
+    let data = {
+        from,
+        to,
+        subject,
+        html: content
+    };
 
-//     return mailgun.messages().send(data);
-// }
+    return mailgun.messages().send(data);
+}
 
-// export { sendEmail };
+export { sendEmail };
